28d4cefaf02fdf5f197b4133e744ce60d7bc6c5a
andrews medina <andrewsmedina@gmail.com>
2011-05-04 14:12:30 -0300

    tests and code for complex and simple patterns

--- alien/alien.py
+++ alien/alien.py
@@ -1,2 +1,32 @@
-def is_alien_word(word, pattern):
+def letter_pattern_groups(pattern):
+    if '(' not in pattern:
+        return list(pattern)
+
+    patterns = []
+    qtd = 0
+    while qtd <= len(pattern)-1:
+        letter = pattern[qtd]
+        if letter != '(':
+            patterns.append(letter)
+            qtd += 1
+        else:
+            p = ''
+            qtd += 1
+            letter = pattern[qtd]
+            while letter != ')':
+                p += letter
+                qtd += 1
+                letter = pattern[qtd]
+            qtd += 1
+            patterns.append(p)
+
+    return patterns
+
+def is_alien_word(word, pattern, length):
+    groups = letter_pattern_groups(pattern)
+
+    for index, letter in enumerate(word):
+        if letter not in groups[index]:
+            return False
+
     return True
--- alien/test_alien.py
+++ alien/test_alien.py
@@ -1,10 +1,31 @@
 import unittest
-from alien import is_alien_word
+from alien import is_alien_word, letter_pattern_groups
+
+class LetterPatternGroupsTestcase(unittest.TestCase):
+
+    def test_simple_pattern_should_return_same_amout_of_letters_how_patters(self):
+        self.assertEqual(3, len(letter_pattern_groups('abc')))
+
+    def test_pattern_with_one_complex_pattern(self):
+        self.assertEqual(3, len(letter_pattern_groups('(ab)bc')))
+
+    def test_pattern_with_full_complex_pattern(self):
+        self.assertEqual(3, len(letter_pattern_groups('(ab)(bc)(cd)')))
 
 
-class MatchInAlienLanguageTestCase(unittest.TestCase):
+class IsAlienWordTestCase(unittest.TestCase):
 
     def test_raw_word_should_be_match_with_itself(self):
-        self.assertTrue(is_alien_word(word='abc', pattern='abc'))
+        self.assertTrue(is_alien_word(word='abc', pattern='abc', length=3))
+
+    def test_other_word_shold_not_be_an_alien_word_if_words_are_different(self):
+        self.assertFalse(is_alien_word(word='abc', pattern='xkc', length=3))
+
+    def test_word_with_pattern_more_complex(self):
+        self.assertTrue(is_alien_word(word='abc', pattern='(ab)bc', length=3))
+
+    def test_word_with_pattern_full_complex(self):
+        self.assertTrue(is_alien_word(word='abc', pattern='(ab)(bx)(cy)', length=3))
+
 
 unittest.main()
