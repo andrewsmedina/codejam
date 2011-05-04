9eabeba0e6839c180d31c0b38dc80dfdbb9cc1c6
andrews medina <andrewsmedina@gmail.com>
2011-05-04 12:52:39 -0300

    add code for reverse words

--- reverse_words/reverse_words.py
+++ reverse_words/reverse_words.py
@@ -1,2 +1,13 @@
 def reverse_words(line):
     return ' '.join(reversed(line.split(' ')))
+
+if __name__ == '__main__':
+    lines = []
+    cases = input()
+
+    for case in range(cases):
+        lines.append(reverse_words(raw_input()))
+
+    for index, line in enumerate(lines):
+        print 'Case #%d: %s' % (index + 1, line)
+
