9aeafd836c403235af8e6c684e1a8745e7eb0d5b
andrews medina <andrewsmedina@gmail.com>
2011-05-04 13:05:07 -0300

    final solution for reverse

--- reverse_words/reverse_words.py
+++ reverse_words/reverse_words.py
@@ -3,11 +3,22 @@ def reverse_words(line):
 
 if __name__ == '__main__':
     lines = []
-    cases = input()
+
+    input_file_name = 'large.in'
+    output_file_name = 'large.out'
+
+    input_file = open(input_file_name)
+    cases = int(input_file.readline().replace('\n', ''))
 
     for case in range(cases):
-        lines.append(reverse_words(raw_input()))
+        lines.append(reverse_words(input_file.readline().replace('\n', '')))
+
+    input_file.close()
+
+    output_file = open(output_file_name, 'w')
 
     for index, line in enumerate(lines):
-        print 'Case #%d: %s' % (index + 1, line)
+        output_file.write('Case #%d: %s\n' % (index + 1, line))
+
+    output_file.close()
 
